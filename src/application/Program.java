71e9847ea56a6bbf27d9053c4d5e7e7dfd8e3e4a
omegahot <leo_omegahot@hotmail.com>
2022-01-27 18:59:30 -0300

    Aula 115. Enumerações

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Order order = new Order(1000, new Date(), OrderStatus.PENDING_PEYMENT);
+		System.out.println(order.toString());
+		
+		System.out.println("-----------------------------------");
+		Order order2 = new Order(1001, new Date(), OrderStatus.valueOf("DELIVERED"));
+		System.out.println(order2);
 	}
 
 }
